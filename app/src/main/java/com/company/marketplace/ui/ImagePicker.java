d79db321ea8377f8e58cd89989e1411b57bd943e
Max <mxcs02@gmail.com>
2022-03-09 02:14:52 +0200

    Make ImagePicker pick multiple images

--- app/src/main/java/com/company/marketplace/ui/ImagePicker.java
+++ app/src/main/java/com/company/marketplace/ui/ImagePicker.java
@@ -17,40 +17,46 @@ import com.company.marketplace.models.ImageOutput;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ImagePicker {
 
-	private final ActivityResultLauncher<String> contentPickerLauncher;
-	private final ActivityResultLauncher<String[]> permissionLauncher;
+	private final ActivityResultLauncher<String> contentPickerLauncher, permissionLauncher;
 
 	@SuppressWarnings("ResultOfMethodCallIgnored")
-	public ImagePicker(Fragment fragment, Consumer<ImageOutput> imageConsumer) {
+	public ImagePicker(Fragment fragment, Integer maxCount, Consumer<List<ImageOutput>> imageConsumer) {
 		Context context = fragment.getContext();
-		contentPickerLauncher = fragment.registerForActivityResult(new ActivityResultContracts.GetContent(),
-			uri -> {
-				try (InputStream stream = Objects.requireNonNull(context).getContentResolver().openInputStream(uri)) {
-					byte[] bytes = new byte[stream.available()];
-					stream.read(bytes);
-					imageConsumer.accept(new ImageOutput("image." + getExtension(context, uri), bytes));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
-		permissionLauncher = fragment.registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
-			result -> {
-				for (boolean isGranted : result.values())
-					if (!isGranted) {
-						Toast.makeText(context, R.string.permission_error, Toast.LENGTH_SHORT).show();
-						return;
+		contentPickerLauncher = fragment.registerForActivityResult(new ActivityResultContracts.GetMultipleContents(),
+			uris -> {
+				if (maxCount != null && uris.size() > maxCount)
+					uris.subList(maxCount, uris.size()).clear();
+				List<ImageOutput> images = new ArrayList<>(uris.size());
+
+				for (Uri uri : uris) {
+					try (InputStream stream = Objects.requireNonNull(context).getContentResolver().openInputStream(uri)) {
+						byte[] bytes = new byte[stream.available()];
+						stream.read(bytes);
+						images.add(new ImageOutput("image." + getExtension(context, uri), bytes));
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
-				contentPickerLauncher.launch("image/*");
+				}
+				imageConsumer.accept(images);
+			});
+		permissionLauncher = fragment.registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+			isGranted -> {
+				if (isGranted)
+					contentPickerLauncher.launch("image/*");
+				else
+					Toast.makeText(context, R.string.permission_error, Toast.LENGTH_SHORT).show();
 			});
 	}
 
-	public void pickImage() {
-		permissionLauncher.launch(new String[] { Manifest.permission.READ_EXTERNAL_STORAGE });
+	public void pickImages() {
+		permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
 	}
 
 	private String getExtension(Context context, Uri uri) {
