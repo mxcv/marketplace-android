75a0584239d2ada90e7f9af4704067d4ce5c0f78
Max <mxcs02@gmail.com>
2022-03-12 17:53:48 +0200

    Refactor strings.xml

--- app/src/main/java/com/company/marketplace/ui/LoginFragment.java
+++ app/src/main/java/com/company/marketplace/ui/LoginFragment.java
@@ -33,7 +33,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 		UserRepository userRepository = new MarketplaceRepositoryFactory(getActivity()).createUserRepository();
 		userRepository.login(emailEditText.getText().toString(), passwordEditText.getText().toString(),
 			ignored -> userRepository.getUser(user -> Account.get().setUser(user, getActivity())),
-			() -> Toast.makeText(getContext(), R.string.invalid_email_or_password, Toast.LENGTH_SHORT).show()
+			() -> Toast.makeText(getContext(), R.string.login_error, Toast.LENGTH_SHORT).show()
 		);
 	}
 }
