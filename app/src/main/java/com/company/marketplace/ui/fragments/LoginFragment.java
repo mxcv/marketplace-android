03953cb67f1bf40e3f3f6b832f33811a0f8b6ffd
Max <mxcs02@gmail.com>
2022-03-20 20:39:25 +0200

    Fix LoginViewModel

--- app/src/main/java/com/company/marketplace/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/company/marketplace/ui/fragments/LoginFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -35,8 +34,6 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 			Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main)
 				.navigate(R.id.nav_items);
 		});
-		loginViewModel.getError().observe(getViewLifecycleOwner(), r ->
-			Toast.makeText(getContext(), R.string.login_error, Toast.LENGTH_SHORT).show());
 
 		return binding.getRoot();
 	}
--- app/src/main/java/com/company/marketplace/ui/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/company/marketplace/ui/viewmodels/LoginViewModel.java
@@ -1,12 +1,14 @@
 package com.company.marketplace.ui.viewmodels;
 
 import android.app.Application;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.company.marketplace.R;
 import com.company.marketplace.models.User;
 import com.company.marketplace.network.repositories.MarketplaceRepositoryFactory;
 import com.company.marketplace.network.repositories.UserRepository;
@@ -14,26 +16,20 @@ import com.company.marketplace.network.repositories.UserRepository;
 public class LoginViewModel extends AndroidViewModel {
 
 	private final MutableLiveData<User> user;
-	private final MutableLiveData<Void> error;
 
 	public LoginViewModel(@NonNull Application application) {
 		super(application);
 		user = new MutableLiveData<>();
-		error = new MutableLiveData<>();
 	}
 
 	public void login(User inputUser) {
 		UserRepository userRepository = new MarketplaceRepositoryFactory(getApplication()).createUserRepository();
 		userRepository.login(inputUser,
 			ignored -> userRepository.getUser(user::setValue),
-			() -> error.setValue(null));
+			() -> Toast.makeText(getApplication(), R.string.login_error, Toast.LENGTH_SHORT).show());
 	}
 
 	public LiveData<User> getUser() {
 		return user;
-	}
-
-	public LiveData<Void> getError() {
-		return error;
 	}
 }
