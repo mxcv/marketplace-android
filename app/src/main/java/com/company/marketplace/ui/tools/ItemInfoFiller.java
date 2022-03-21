e01a8df0eb01e947e57dd07d98d383423f4bc6f9
Max <mxcs02@gmail.com>
2022-03-21 22:44:03 +0200

    Revert "Fix filler"

--- app/src/main/java/com/company/marketplace/ui/tools/ItemInfoFiller.java
+++ app/src/main/java/com/company/marketplace/ui/tools/ItemInfoFiller.java
@@ -20,39 +20,39 @@ public class ItemInfoFiller {
 	}
 
 	public ItemInfoFiller fillCategories(List<Category> categories) {
-		for (Item item : items)
-			if (item.getCategory() != null) {
-				item.setCategory(categories.stream()
-					.filter(c -> c.getId() == item.getCategory().getId())
+		items.stream()
+			.filter(i -> i.getCategory() != null)
+			.forEach(i -> i.setCategory(
+				categories.stream()
+					.filter(c -> c.getId() == i.getCategory().getId())
 					.findFirst()
-					.orElse(item.getCategory()));
-			}
+					.orElse(i.getCategory())));
 
 		return this;
 	}
 
 	public ItemInfoFiller fillCurrencies(List<Currency> currencies) {
-		for (Item item : items)
-			if (item.getCurrency() != null) {
-				item.setCurrency(currencies.stream()
-					.filter(c -> c.getId() == item.getCurrency().getId())
+		items.stream()
+			.filter(i -> i.getCurrency() != null)
+			.forEach(i -> i.setCurrency(
+				currencies.stream()
+					.filter(c -> c.getId() == i.getCurrency().getId())
 					.findFirst()
-					.orElse(item.getCurrency()));
-			}
+					.orElse(i.getCurrency())));
 
 		return this;
 	}
 
 	public ItemInfoFiller fillCities(List<Country> countries) {
-		for (Item item : items)
-			if (item.getUser() != null && item.getUser().getCity() != null) {
-				item.getUser().setCity(countries.stream()
+		items.stream()
+			.filter(i -> i.getUser() != null && i.getUser().getCity() != null)
+			.forEach(i -> i.getUser().setCity(
+				countries.stream()
 					.flatMap(c -> c.getRegions().stream())
 					.flatMap(r -> r.getCities().stream())
-					.filter(c -> c.getId() == item.getUser().getCity().getId())
+					.filter(c -> c.getId() == i.getUser().getCity().getId())
 					.findFirst()
-					.orElse(item.getUser().getCity()));
-			}
+					.orElse(i.getUser().getCity())));
 
 		return this;
 	}
