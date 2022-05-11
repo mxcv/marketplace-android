371515996ec40e8633a00d134042a21a962d2ff9
Max <mxcs02@gmail.com>
2022-05-11 17:22:30 +0300

    Add pagination to feedback loading

--- app/src/main/java/com/company/marketplace/ui/fragments/UserFeedbackFragment.java
+++ app/src/main/java/com/company/marketplace/ui/fragments/UserFeedbackFragment.java
@@ -6,21 +6,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.company.marketplace.R;
 import com.company.marketplace.databinding.FragmentUserFeedbackBinding;
+import com.company.marketplace.models.Feedback;
 import com.company.marketplace.models.User;
 import com.company.marketplace.ui.adapters.FeedbackAdapter;
 import com.company.marketplace.ui.tools.UserInfoSetter;
 import com.company.marketplace.ui.viewmodels.SelectedUserViewModel;
 import com.company.marketplace.ui.viewmodels.UserFeedbackViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class UserFeedbackFragment extends Fragment {
 
 	private FragmentUserFeedbackBinding binding;
+	private List<Feedback> feedback;
 
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,11 +49,27 @@ public class UserFeedbackFragment extends Fragment {
 	private void setUser(User user) {
 		UserInfoSetter.setUser(requireContext(), binding.userFeedbackUser, user);
 
-		new ViewModelProvider(this)
-			.get(UserFeedbackViewModel.class)
-			.getItems(user)
-			.observe(getViewLifecycleOwner(), feedback -> {
-				binding.userFeedbackRecyclerView.setAdapter(new FeedbackAdapter(requireContext(), feedback));
+		feedback = new ArrayList<>();
+		binding.userFeedbackRecyclerView.setAdapter(new FeedbackAdapter(requireContext(), feedback));
+		UserFeedbackViewModel userFeedbackViewModel = new ViewModelProvider(this).get(UserFeedbackViewModel.class);
+
+		if (userFeedbackViewModel.getFeedback().getValue() != null)
+			addFeedback(userFeedbackViewModel.getFeedback().getValue());
+		else
+			userFeedbackViewModel.loadNextPage(user);
+
+		userFeedbackViewModel.getLastPage().observe(getViewLifecycleOwner(), page -> addFeedback(page.getItems()));
+
+		binding.userFeedbackScrollView.setOnScrollChangeListener(
+			(NestedScrollView.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+				if (scrollY > oldScrollY && scrollY >= (binding.userFeedbackRecyclerView.getMeasuredHeight() - v.getMeasuredHeight()))
+					userFeedbackViewModel.loadNextPage(user);
 			});
+	}
+
+	private void addFeedback(List<Feedback> newFeedback) {
+		int start = feedback.size();
+		feedback.addAll(newFeedback);
+		Objects.requireNonNull(binding.userFeedbackRecyclerView.getAdapter()).notifyItemRangeInserted(start, newFeedback.size());
 	}
 }
--- app/src/main/java/com/company/marketplace/ui/viewmodels/UserFeedbackViewModel.java
+++ app/src/main/java/com/company/marketplace/ui/viewmodels/UserFeedbackViewModel.java
@@ -1,6 +1,7 @@
 package com.company.marketplace.ui.viewmodels;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -8,33 +9,59 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.company.marketplace.models.Feedback;
+import com.company.marketplace.models.Page;
 import com.company.marketplace.models.User;
-import com.company.marketplace.network.repositories.FeedbackRepository;
 import com.company.marketplace.network.repositories.MarketplaceRepositoryFactory;
 
 import java.util.List;
 
 public class UserFeedbackViewModel extends AndroidViewModel {
 
-	private MutableLiveData<List<Feedback>> items;
+	private static final int PAGE_SIZE = 20;
+	private final MutableLiveData<List<Feedback>> feedback;
+	private final MutableLiveData<Page<Feedback>> lastPage;
+	private boolean isLoading;
 
 	public UserFeedbackViewModel(@NonNull Application application) {
 		super(application);
+		feedback = new MutableLiveData<>();
+		lastPage = new MutableLiveData<>();
 	}
 
-	public LiveData<List<Feedback>> getItems(User user) {
-		if (items == null) {
-			items = new MutableLiveData<>();
-			loadItems(user);
+	public LiveData<List<Feedback>> getFeedback() {
+		return feedback;
+	}
+
+	public MutableLiveData<Page<Feedback>> getLastPage() {
+		return lastPage;
+	}
+
+	public synchronized void loadNextPage(User user) {
+		if (!isLoading && hasMorePages()) {
+			isLoading = true;
+			new MarketplaceRepositoryFactory(getApplication())
+				.createFeedbackRepository()
+				.getFeedback(
+					user.getId(),
+					lastPage.getValue() == null ? 1 : lastPage.getValue().getPageIndex() + 1,
+					PAGE_SIZE,
+					page -> {
+						Log.d("feedback", "Feedback loaded: " + page.getItems().size());
+						Log.d("feedback", String.format("Page: %d/%d", page.getPageIndex(), page.getTotalPages()));
+						if (feedback.getValue() == null)
+							feedback.setValue(page.getItems());
+						else {
+							feedback.getValue().addAll(page.getItems());
+							feedback.setValue(feedback.getValue());
+						}
+						lastPage.setValue(page);
+						isLoading = false;
+					});
 		}
-		return items;
 	}
 
-	private void loadItems(User user) {
-		FeedbackRepository feedbackRepository = new MarketplaceRepositoryFactory(getApplication())
-			.createFeedbackRepository();
-		feedbackRepository.getFeedback(user.getId(), 1, 20, page -> {
-			items.setValue(page.getItems());
-		});
+	private boolean hasMorePages() {
+		Page<Feedback> lastPageValue = lastPage.getValue();
+		return lastPageValue == null || lastPageValue.getPageIndex() != lastPageValue.getTotalPages();
 	}
 }
